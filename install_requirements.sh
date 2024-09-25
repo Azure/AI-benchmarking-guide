2b37664b3060ea251e0a0c87f77b86ce81b64fce
gitaumark <156354973+gitaumark@users.noreply.github.com>
2024-09-25 17:18:45 -0400

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -16,9 +16,12 @@ pip3 install --no-cache-dir prettytable
 
 sudo apt install fio
 
+sleep 4
+
 # Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install ca-certificates curl
+sleep 2
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -28,6 +31,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sleep 1
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
@@ -37,7 +41,7 @@ sudo usermod -aG docker $USER
 newgrp docker
 docker run hello-world
 
-
+sleep 2
 
 # NCCL Install
 git clone https://github.com/NVIDIA/nccl
